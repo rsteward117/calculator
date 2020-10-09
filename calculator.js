8b3d82287d8a91af497a44becf0d2a982178a303
rsteward117 <66583624+rsteward117@users.noreply.github.com>
2020-10-08 20:56:41 -0500

    Delete calculator.js

--- calculator.js
+++
@@ -1,7 +0,0 @@
-//wire the buttons to js code and make them do something.
-//create a variable that takes 2 user inputs.
-//
-
-let display = document.getElementById("display");
-display.innerHTML = "0";
-
